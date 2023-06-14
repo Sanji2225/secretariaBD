9de7cb228a37b85dff0003db3137c058d9744471
Wayne <lucaswayne779@gmail.com>
2023-06-14 15:29:53 -0300

    terminei os models

--- secretaria/admin.py
+++ secretaria/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from secretaria.models import aluno
+from secretaria.models import professor
+from secretaria.models import disciplina
+from secretaria.models import orienta
+from secretaria.models import cursar
 # Register your models here.
+
+admin.site.register(aluno)
+
+admin.site.register(professor)
+
+admin.site.register(disciplina)
+
+admin.site.register(orienta)
+
+admin.site.register(cursar)
--- secretaria/models.py
+++ secretaria/models.py
@@ -1,21 +1,77 @@
 from django.db import models
 
-class professor(models.Model):
-    MASP=models.IntegerField(max_length=11)
-    dep=models.CharField(max_length=3)
-    nomep=models.CharField(max_length=30)
-    sexop=[('M'), ('F')]
-    titulo=models.CharField(max_length=2)
+
+sexo =     [('M', 'Masculino'), 
+            ('F', 'Feminino')]
 
 class aluno(models.Model):
-    matricula=models.IntegerField(max_length=11)
+    matricula=models.IntegerField(primary_key=True)
     nomea=models.CharField(max_length=30)
-    sexoa= [('M'), ('F')]
+    sexoa= models.CharField(max_length=1, choices=sexo)
     curso=models.CharField(max_length=30)
     cpf=models.CharField(max_length=11)
     endereco=models.CharField(max_length=60)
     celular=models.CharField(max_length=11)
 
-#class disciplina(models.Model):
- #   cod_disc=models.IntegerField(max_length=9)
-  #  MASP=models.ForeignKey
+
+    def __str__(self) -> str:
+        return self.nomea
+    class Meta:
+        managed=False
+        db_table='aluno'
+        verbose_name='Aluno'
+class professor(models.Model):
+    masp= models.IntegerField(primary_key=True, null=False)
+    nomep= models.CharField(max_length=30, null=False)
+    dep = models.CharField(max_length=3)
+    sexop= models.CharField(max_length=1, choices=sexo)
+    
+    def __str__(self) -> str:
+        return self.nomep
+    class Meta:
+        managed=False
+        db_table='professor'
+        verbose_name='Professore'
+
+
+class disciplina(models.Model):
+    class per(models.IntegerChoices):
+        Primeiro = 1
+        Segundo = 2        
+    
+    cod_disc=models.IntegerField(primary_key=True, null=False)
+    masp=models.ForeignKey(professor, null=False, on_delete=models.CASCADE)
+    cargaHP=models.IntegerField
+    cargaHT=models.IntegerField
+    periodo=models.IntegerField(choices=per.choices)
+    nomed=models.CharField(max_length=40, null=False)
+
+    def __str__(self) -> str:
+        return self.nomed
+    class Meta:
+        managed=False
+        db_table='disciplina'
+        verbose_name='Disciplina'
+
+class orienta(models.Model):
+    cod_o = models.ForeignKey(professor, null=False, on_delete=models.CASCADE)
+    matricula_o=models.ForeignKey(aluno, null=False, on_delete=models.CASCADE, primary_key=True)
+
+
+    def __str__(self) -> str:
+        return self.matricula_o
+    class Meta:
+        managed=False
+        db_table='orienta'
+        verbose_name='Orientações'
+    
+class cursar(models.Model):
+    cod_c=models.ForeignKey(disciplina, on_delete=models.CASCADE, primary_key=True, null=False)
+    cod_a=models.ForeignKey(aluno, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.matricula_o
+    class Meta:
+        managed=False
+        db_table='cursar'
+        verbose_name='Matriculas em Disciplina'
